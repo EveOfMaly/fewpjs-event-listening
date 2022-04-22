d5f248fc625490701ad28334afa8eaf654e7514b
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-21 22:09:01 -0400

    commit tree 3bc730f949ae1c059234b1310dd37aa58e1ff2c3

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input = document.getElementById('input'); //the node that will be oing the listening. Store it in input
+    input.addEventListener('click', function(event) { //evoke addeventlistener. event name is click and second argument is callbackfunction
+    alert('I was clicked!');
+});
 }
+
+addingEventListener();
